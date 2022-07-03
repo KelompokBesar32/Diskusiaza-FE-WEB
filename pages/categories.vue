897d71811a94f3209a0f83fc6947d74fc957573b
Dini Dwi Andayani <dini.handayani.141326@gmail.com>
2022-07-03 19:28:09 +0700

    register dan categories

---
+++ pages/categories.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="categories">
+    <!-- <div class="container">
+      <h3>Pilih ruang baca yang kammu sukai</h3>
+      <div class="row">
+        <div class="card" style="width: 15rem; text-align: center">
+          <img src="../assets/login.png" class="card-img-top" alt="" />
+          <div class="card-body">
+            <a href="#" class="btn btn-primary">Add to Cart</a>
+          </div>
+        </div>
+        <div class="card" style="width: 15rem; text-align: center">
+          <img src="../assets/login.png" class="card-img-top" alt="" />
+          <div class="card-body">
+            <a href="#" class="btn btn-primary">Add to Cart</a>
+          </div>
+        </div>
+        <div class="card" style="width: 15rem; text-align: center">
+          <img src="../assets/login.png" class="card-img-top" alt="" />
+          <div class="card-body">
+            <a href="#" class="btn btn-primary">Add to Cart</a>
+          </div>
+        </div>
+        <div class="card" style="width: 15rem; text-align: center">
+          <img src="../assets/login.png" class="card-img-top" alt="" />
+          <div class="card-body">
+            <a href="#" class="btn btn-primary">Add to Cart</a>
+          </div>
+        </div>
+        <div class="card" style="width: 15rem; text-align: center">
+          <img src="../assets/login.png" class="card-img-top" alt="" />
+          <div class="card-body">
+            <a href="#" class="btn btn-primary">Add to Cart</a>
+          </div>
+        </div>
+        <div class="card" style="width: 15rem; text-align: center">
+          <img src="../assets/login.png" class="card-img-top" alt="" />
+          <div class="card-body">
+            <a href="#" class="btn btn-primary">Add to Cart</a>
+          </div>
+        </div>
+      </div>
+    </div> -->
+    <div class="container">
+      <h8 class="title">Pilih ruang baca yang kamu sukai</h8>
+      <div class="products-container">
+        <div class="product" data-name="p-1">
+          <img
+            src="../assets/science.jpg"
+            alt=""
+            style="height: 100px; width: 75px"
+          />
+          <button type="button" class="btn1">Sains & Teknologi</button>
+        </div>
+        <div class="product" data-name="p-2">
+          <img
+            src="../assets/politic.jpg"
+            alt=""
+            style="height: 100px; width: 75px"
+          />
+          <button type="button" class="btn1">Ilmu Politik</button>
+        </div>
+        <div class="product" data-name="p-3">
+          <img
+            src="../assets/broke.jpg"
+            alt=""
+            style="height: 100px; width: 75px"
+          />
+          <button type="button" class="btn1">Patah Hati</button>
+        </div>
+        <div class="product" data-name="p-4">
+          <img
+            src="../assets/psikolog.jpg"
+            alt=""
+            style="height: 100px; width: 75px"
+          />
+          <button type="button" class="btn1">Psikologi Hidup</button>
+        </div>
+        <div class="product" data-name="p-5">
+          <img
+            src="../assets/pasangan.jpg"
+            alt=""
+            style="height: 100px; width: 75px"
+          />
+          <button type="button" class="btn1">Pasangan</button>
+        </div>
+        <div class="product" data-name="p-6">
+          <img
+            src="../assets/dinamika.jpg"
+            alt=""
+            style="height: 100px; width: 75px"
+          />
+          <button type="button" class="btn1">Dinamika Sosial</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CategoriesPages',
+  layout: 'auth',
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  /* outline: none;
+  border: none; */
+  transition: all 0.2s linier;
+  text-transform: capitalize;
+}
+.categories {
+  font-size: 60%;
+  overflow-x: hidden;
+  background: #fff;
+}
+.body {
+  background: #fff;
+}
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 3rem 2rem;
+  background-color: #fff;
+}
+.title {
+  font-size: 2rem;
+  color: #444;
+  margin-bottom: 4rem;
+  text-align: center;
+  display: block;
+  margin: auto;
+}
+.products-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
+  gap: 2rem;
+}
+.product {
+  text-align: center;
+  padding: 3rem 2rem;
+  background: #fff;
+
+  cursor: pointer;
+  height: 175px;
+  width: 150px;
+
+  display: block;
+  margin: auto;
+}
+
+.product:hover .img {
+  transform: scale(0.9);
+}
+.btn1 {
+  margin-top: 25%;
+  border-radius: 30%;
+  margin-bottom: 50%;
+}
+</style>
--- pages/register.vue
+++ pages/register.vue
@@ -1,5 +1,113 @@
 <template>
-  <div></div>
+  <div>
+    <section class="form my-4 mx-5">
+      <div class="container">
+        <div class="row no-gutters">
+          <div class="col-lg-12 px-5 pt-5">
+            <h3>Daftar</h3>
+            <form>
+              <div class="row">
+                <div class="col-6">
+                  <input
+                    type="text"
+                    placeholder="Nama Depan"
+                    class="form-control my-3 p-4"
+                  />
+                </div>
+                <div class="col-6">
+                  <input
+                    type="text"
+                    placeholder="Nama Belakang"
+                    class="form-control my-3 p-4"
+                  />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12">
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    class="form-control my-3 p-4"
+                  />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-6">
+                  <input
+                    type="Password"
+                    placeholder="Password"
+                    class="form-control my-3 p-4"
+                  />
+                </div>
+                <div class="col-6">
+                  <input
+                    type="Password"
+                    placeholder="Confirm Password"
+                    class="form-control my-3 p-4"
+                  />
+                </div>
+              </div>
+              <h4>Tanggal Lahir</h4>
+              <div class="row">
+                <div class="col-3">
+                  <input
+                    type="date"
+                    placeholder="day"
+                    class="form-control my-3 p-4"
+                  />
+                </div>
+                <div class="col-3">
+                  <input
+                    type="date"
+                    placeholder="month"
+                    class="form-control my-3 p-4"
+                  />
+                </div>
+                <div class="col-3">
+                  <input
+                    type="date"
+                    placeholder="year"
+                    class="form-control my-3 p-4"
+                  />
+                </div>
+                <div class="col-3"></div>
+              </div>
+
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio1"
+                  value="option1"
+                />
+                <label class="form-check-label" for="inlineRadio1"
+                  >Laki-laki</label
+                >
+              </div>
+              <div class="form-check form-check-inline">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio2"
+                  value="option2"
+                />
+                <label class="form-check-label" for="inlineRadio2"
+                  >Perempuan</label
+                >
+              </div>
+              <div class="row">
+                <div class="col-12">
+                  <button type="button" class="btn1 mt-3 mb-5">Login</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -9,4 +117,33 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  /* background-color: blue; */
+}
+body {
+  background: white;
+}
+.row {
+  background: #cce4f8;
+  border-radius: 30px;
+}
+.btn1 {
+  border: none;
+  outline: none;
+  height: 50px;
+  width: 100%;
+  background-color: blue;
+  color: white;
+  border-radius: 25px;
+  font-weight: bold;
+}
+.btn1:hover {
+  background: black;
+  border: 1px solid;
+  color: white;
+}
+</style>
