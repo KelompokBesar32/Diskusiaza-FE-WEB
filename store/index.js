c2bb59d300c05e466eed6cf575a8eb43079f4800
albertdimasa <albertabraham28@gmail.com>
2022-07-08 17:47:22 +0700

    change proxy to use API in local

--- store/index.js
+++ store/index.js
@@ -11,7 +11,7 @@ export const mutations = {
 export const actions = {
   async SIGN_UP({ commit }, payload) {
     try {
-      await this.$axios.post('/auth/register', payload)
+      await this.$axios.post('/api/auth/register', payload)
       this.$swal.fire({
         icon: 'success',
         title: 'Pendaftaran Berhasil',
@@ -25,7 +25,7 @@ export const actions = {
 
   async SIGN_IN({ commit }, payload) {
     try {
-      const res = await this.$axios.post('/auth/login', payload)
+      const res = await this.$axios.post('/api/auth/login', payload)
       commit('SET_USER', res.data.token)
       this.$router.push({ path: '/' })
     } catch (e) {
